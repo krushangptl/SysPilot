0776739277599a1ac8e639034e6dee606554162b
Krushang D Patel <krushang4201@gmail.com>
2025-10-23 16:32:40 +0530

    change init to main

---
+++ sys/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import os
+from rich.console import Console
+from rich.table import Table
+
+console = Console()
+
+def get_cpu_info:
+    cpu_percent = psutil.cpu_percent()
+
